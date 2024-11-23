f2b0755197408b385c46cc22a1f356917f5a987c
BreakerTW Security <breaker@localhost.localdomain>
2024-11-23 14:21:22 -0500

    firt commit

--- static/js/script.js
+++ static/js/script.js
@@ -3,16 +3,16 @@ const toggleSwitch = document.getElementById('theme-toggle');
 
         toggleSwitch.addEventListener('change', () => {
             if (toggleSwitch.checked) {
-                body.classList.remove('light-theme');
-                body.classList.add('dark-theme');
-            } else {
                 body.classList.remove('dark-theme');
                 body.classList.add('light-theme');
+            } else {
+                body.classList.remove('light-theme');
+                body.classList.add('dark-theme');
             }
         });
         // Initialize the default mode
         if (!localStorage.getItem('theme')) {
-            localStorage.setItem('theme', 'dark-theme');
+            localStorage.setItem('theme', 'light-theme');
         }
 
 const navBtn = document.querySelector('#menu-icon');
